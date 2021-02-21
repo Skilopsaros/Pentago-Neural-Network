da2537eb8853112a90e269091383d3b66349672d
skilopsaros <skilopsaros@gmail.com>
2021-02-21 01:36:47 +0000

    debugging and some print statments

--- network_class.py
+++ network_class.py
@@ -11,7 +11,7 @@ class network:
         Vs is a list of 1-D arrays that will work as the biases.'''
         self.M = Ms
         self.V = Vs
-        self.rating = 0
+        self.score = 0
 
     def hypertan(self,list):
         ''' A hyperbolic tangent function that accepts a list and returns a list of the hyperbolic tangent of each element
@@ -66,19 +66,19 @@ class network:
         '''returns the weights of the network'''
         return(self.M,self.V)
 
-    def calculate_move(self,board):
-        ''' this function will take the board object from board.py, translate it into a valid input for calculate_output,
+    def calculate_move(self,game):
+        ''' this function will take the game object from board.py, translate it into a valid input for calculate_output,
          then make the move on the board based on the output'''
         input = np.zeros(39)# the network will take 39 inputs
-        input[0] = float(board.turn)/36 # the first will be a normalised turn number, how many turns have been played
+        input[0] = float(game.board.turn)/36 # the first will be a normalised turn number, how many turns have been played
          # for the second, we will look at the centre of each board, count the free ones, normalise it and use it as the second input
         for i in range(2):
              for j in range(2):
-                 if 0 == board.subs[i][j].contents[1][1]:
+                 if 0 == game.board.subs[i][j].contents[1][1]:
                      input[1] += 0.25
-        input[2] = -2*(board.turn%2)+1 # this will be 1 if the network is player 1 or -1 if it's player 2
+        input[2] = -2*(game.board.turn%2)+1 # this will be 1 if the network is player 1 or -1 if it's player 2
         for i in range(36): # now add the contents of the board into the next 36 inputs
-            number = board.subs[(i//18)%2][(i//3)%2].contents[(i//6)%3][(i%3)]
+            number = game.board.subs[(i//18)%2][(i//3)%2].contents[(i//6)%3][(i%3)]
             if input[2] == number: # 1 if it is a stone that belongs to the network
                 input[i] = 1
             elif 0 == number: # 0 of it is empty
@@ -87,43 +87,42 @@ class network:
                 input[i] =-1
 
         # Now that the input is ready, it's time to calculate the outputs
-        outputs = calculate_output(input)
+        outputs = self.calculate_output(input)
         # Now to convert the outputs into a valid move
-
+        #print(outputs)
         move_x = int(outputs[0]*3+4) # the x component of the move, take the -1 to 1 range, and convert it into an int between 1 and 6
         if 7 == move_x: #edge case when outputs[0]=1.0
             move_x=6 # the y component of the move, take the -1 to 1 range, and convert it into an int between 1 and 6
-
+        #print('move x is '+str(move_x))
         move_y = int(outputs[1]*3+4)
         if 7 == move_y: #edge case when outputs[1]=1.0
             move_y=6
-
+        #print('move y is '+str(move_y))
         if outputs[2]>0: # the rotational direction component of the move, if positive do clockwise, otherwise anticlockwise
             move_r = 'C'
         else:
             move_r = 'A'
-
+        #print('move r is '+move_r)
         if outputs[3]>0:# the x component of which sub-board to rotate, positive for 2, negative for 1
-            move_rx = 2
-        else:
             move_rx = 1
-
-        if outputs[4]>0:# the y component of which sub-board to rotate, positive for 2, negative for 1
-            move_ry = 2
         else:
+            move_rx = 0
+        #print('move rx is '+str(move_rx))
+        if outputs[4]>0:# the y component of which sub-board to rotate, positive for 2, negative for 1
             move_ry = 1
-
+        else:
+            move_ry = 0
+        #print('move rx is '+str(move_ry))
         # Now to atempt to make a move
         #first we try the stone placement. if the move is invalid, then place randomly
         random_move_pool = [1,2,3,4,5,6]
         failed = 0
-        while 0 == board.first_half_round(move_x,move_y): # Each time the code will atempt to change the board. if the move is valid, it will skip the loop.
+        while 0 == game.first_half_round(move_x,move_y): # Each time the code will atempt to change the board. if the move is valid, it will skip the loop.
             failed = 1 # recording that it failed at least once
+            #print('random move')
             move_x = random.choice(random_move_pool) # if the move is not valid it will try to get new x and y values
             move_y = random.choice(random_move_pool) # and by checking if these are valid, every time it will atempt to change the board
-        self.score += (-30) #punishing the netwoork for bad play
-
+        self.score += (-30)*failed #punishing the netwoork for bad play
+        #print(self.score)
         # thankfully, there are no invalid possible moves for the second move.
-        return(board.second_half_round(move_rx,move_ry,move_r)) #returns the win state of the game
-
-        pass
+        return(game.second_half_round(move_rx,move_ry,move_r)) #returns the win state of the game
